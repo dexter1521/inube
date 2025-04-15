43bf6e8ba0c44cd9c23a4cdc1c3630204b99ffdc
Eduardo Marvil <emtv2126@gmail.com>
2025-04-15 15:22:00 -0600

    se crea un controlador base para respuestas  genrales del api

---
+++ app/Controllers/Api/BaseApiController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controllers\Api;
+
+use CodeIgniter\RESTful\ResourceController;
+
+class BaseApiController extends ResourceController
+{
+    protected function successResponse($message, $data = [], $code = 200)
+    {
+        return $this->respond([
+            'status'  => true,
+            'message' => $message,
+            'data'    => $data
+        ], $code);
+    }
+
+    protected function errorResponse($message, $errors = [], $code = 400)
+    {
+        return $this->respond([
+            'status'  => false,
+            'message' => $message,
+            'errors'  => $errors
+        ], $code);
+    }
+
+    protected function invalidJsonResponse()
+    {
+        return $this->errorResponse('JSON no válido: ' . json_last_error_msg(), [], 400);
+    }
+
+    protected function emptyJsonResponse()
+    {
+        return $this->errorResponse('Datos JSON inválidos o vacíos', [], 400);
+    }
+}
